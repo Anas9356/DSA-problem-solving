cdf8944440a695b387766e5bc302b5587d43b2c1
Anas9356 <98730862+Anas9356@users.noreply.github.com>
2026-01-18 15:45:00 +0530

    Update Longest Substring Without Repeating Characters(Sliding Window).py

--- DSA DAY3/Longest Substring Without Repeating Characters(Sliding Window).py
+++ DSA DAY3/Longest Substring Without Repeating Characters(Sliding Window).py
@@ -12,3 +12,32 @@ class Solution:
             max_len = max(max_len, right - left + 1)
 
         return max_len
+
+self approch
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        Mx=0
+        Cnt=0
+        start=0
+        myD=set()
+        i=0
+        while i < len(s):
+            # add in dictionary and increase freq
+            if s[i] in myD:
+                start+=1
+                i=start
+                myD.clear()
+                Mx=max(Cnt,Mx)
+                myD.add(s[i])
+                Cnt=1
+                i+=1
+            else:
+                myD.add(s[i])
+                Cnt+=1
+                Mx=max(Cnt,Mx)
+                i+=1
+
+        return Mx
+
+
+
